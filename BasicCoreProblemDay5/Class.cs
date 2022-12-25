caadb8f23e4a838134449e25b8943166cf1191f5
Shree Thakre <shreet365@gmail.com>
2022-12-25 12:50:50 +0530

    Check Leap Year

---
+++ BasicCoreProblemDay5/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblemDay5
+{
+    class Class
+    {
+        public void LeapYear(int iCnt)
+        {
+            if (((iCnt % 4 == 0) && (iCnt % 100 != 0)) || (iCnt % 400 == 0))
+            {
+                Console.WriteLine("It is a leap Year ");
+            }
+            else
+            {
+                Console.WriteLine("It is a Not leap Year ");
+            }
+        }
+    }
+}
--- BasicCoreProblemDay5/Program.cs
+++ BasicCoreProblemDay5/Program.cs
@@ -12,28 +12,10 @@ namespace BasicCoreProblemDay5
     {
         static void Main(string[] args)
         {
-            int iTail = 0;
-            int iHead = 0;
-            Random random = new Random();
-            Console.WriteLine("Enter number how many time flip the Coin");
-            int iNumber = Convert.ToInt32(Console.ReadLine());
-            for(int i=1;i<=iNumber;i++)
-            {
-                Double check = random.NextDouble();
-                if(check < 0.5)
-                {
-                    iTail++;
-                }
-                else
-                {
-                    iHead++;
-                }
-               
-            }
-            double percentage = (iTail * 100)/ iNumber;
-            Console.WriteLine(" Tail is{0} and its percentage is : {1} ",iTail,percentage);
-            double percentage1 = (iHead * 100)/ iNumber;
-            Console.WriteLine("Head is{0} and its percentage is  :{1} " ,iHead,percentage1);
+            Console.WriteLine("Enter yaer ");
+            int iCnt = Convert.ToInt32(Console.ReadLine());
+            Class obj = new Class();   // create object
+            obj.LeapYear(iCnt);        //
             Console.ReadLine();
             
         }
